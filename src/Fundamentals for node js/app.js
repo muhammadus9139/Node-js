2b695b689ed6856016882ab679375aca73943c20
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-09 19:12:48 +0500

    Add basic JavaScript examples in app.js
    This file contains basic JavaScript examples including variables, if-else statements, functions, loops, arrays, and objects.

---
+++ src/Fundamentals for node js/app.js
@@ -0,0 +1,56 @@
+
+// Basic variables
+var a=10;
+let b=20;
+
+b=100
+a=100
+console.log(a+b);
+
+// Basic js if-else
+if(a==100){
+    console.log("ok");
+}
+else{
+    console.log("not ok");
+    
+}
+
+//  Basic js functions
+function fruit() {
+    console.log("this is :"+fruit);   
+}
+
+console.log("banana");
+console.log("apple");
+
+
+// For loops
+for (var a=1;a<=10;a++){
+    console.log(a);
+}
+
+
+//  while loop js
+var a=0
+while(a<=10){
+     console.log(a);
+    a++
+}
+
+
+// Arrays
+var users=["usama","ali","ahmad"]
+for(var a=0;a<users.length;a++){
+    console.log(users[a]);   
+}
+
+// Objects
+var user={
+    name:"usama",
+    age:20,
+    city:"lhr"
+}
+console.log(user);
+
+
