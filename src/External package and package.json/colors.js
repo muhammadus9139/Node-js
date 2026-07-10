7ddedb455fa0804146bf275c64c94c0c7b35f53c
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-10 15:27:54 +0500

    Create colors.js

---
+++ src/External package and package.json/colors.js
@@ -0,0 +1,8 @@
+// External package or third party modules
+// install it with commad npm install colors
+
+const colors=require('colors');
+console.log(colors.red('hi this is usama'));
+console.log(colors.green('hi this is usama'));
+console.log(colors.blue('hi this is usama'));
+
