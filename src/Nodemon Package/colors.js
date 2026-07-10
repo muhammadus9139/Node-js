7d151ae09b10a685347f7074d6ea8acbc3c99d87
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-10 15:53:44 +0500

    Create colors.js for colored console output
    Add colors.js to demonstrate color output using the colors package.

---
+++ src/Nodemon Package/colors.js
@@ -0,0 +1,8 @@
+// install nodeman , use to run it continuosly
+// by command node i nodeman
+// and run by nodeman color.js
+const colors=require('colors');
+console.log(colors.red('hi this is usama'));
+console.log(colors.green('hi this is usama'));
+console.log(colors.blue('hi this is usama'));
+
