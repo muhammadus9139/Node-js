34207b546a5ed3e379690ef5dd6c0a109edc6df8
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-13 16:47:33 +0500

    Create home.js

---
+++ Express js/src/use ES import and export/pages/home.js
@@ -0,0 +1,10 @@
+
+function home(){
+    return "<h1> Home page </h1>"
+}
+
+// this export is commonJS type export
+// module.exports=home;
+
+// this export is module js type export 
+export default home;
