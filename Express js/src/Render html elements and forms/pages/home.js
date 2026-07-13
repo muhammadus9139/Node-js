9afdb257889a8a5c1accf0198a17ac5426417ce7
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-13 17:29:59 +0500

    Create home.js

---
+++ Express js/src/Render html elements and forms/pages/home.js
@@ -0,0 +1,10 @@
+
+function home(){
+    return "<h1> Home page </h1> <a href='/login'> Go to login </a> ";
+}
+
+// this export is commonJS type export
+// module.exports=home;
+
+// this export is module js type export 
+export default home;
