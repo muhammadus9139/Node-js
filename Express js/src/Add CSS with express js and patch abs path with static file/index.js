02ef4ec3e719bd95306ef0b9fedf66cecf0d4e77
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-14 14:54:52 +0500

    Create index.js

---
+++ Express js/src/Add CSS with express js and patch abs path with static file/index.js
@@ -0,0 +1,42 @@
+
+import express from "express";
+import path from 'path';
+import home from "./pages/home.js";
+
+const app=express();
+   // here we are patching absolute path with static file (css file)
+        const publicpath=path.resolve('public')
+        app.use(express.static(publicpath))
+
+    app.get("/",(req,resp)=>{ 
+        // its the way to find absolute path
+        const absPath=path.resolve('view/home.html');
+        resp.sendFile(absPath)
+    })
+
+    app.get("/login",(req,resp)=>{
+
+        // here we are patching absolute path with static file (css file)
+        const publicpath=path.resolve('public')
+        app.use(express.static(publicpath))
+
+        const absPath=path.resolve('view/login.html');
+        // sendFile is used to connect a whole html file
+        resp.sendFile(absPath)
+    })
+
+    app.get("/about",(req,resp)=>{
+
+        // here we are patching absolute path with static file (css file)
+        const publicpath=path.resolve('public')
+        app.use(express.static(publicpath))
+
+        const absPath=path.resolve('view/about.html');
+        resp.sendFile(absPath)
+    })
+    
+    app.use((req,resp)=>{
+        const absPath=path.resolve('view/404.html')
+        resp.status(404).sendFile(absPath)
+    })
+app.listen(3400);
