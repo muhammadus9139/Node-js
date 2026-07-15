04d8da482d72d7c95c423105c5efafeda760bbed
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-15 22:17:43 +0500

    Update index.js

--- Express js/src/API example with dynamic route/index.js
+++ Express js/src/API example with dynamic route/index.js
@@ -8,6 +8,7 @@ app.get("/",(req,resp)=>{
    console.log(userdata);
 })
 
+// yaha hm url ma id se check kryn gy 1, 2 etc or specific user detail jo api ma ha wo show ho gi
 app.get("/user/:id",(req,resp)=>{
    const id=req.params.id;
    
