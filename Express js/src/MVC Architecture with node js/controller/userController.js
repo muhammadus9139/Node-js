3d8238bff0aaa9d763d2504e3ec8d7900931ae64
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-15 19:04:18 +0500

    Update userController.js

--- Express js/src/MVC Architecture with node js/controller/userController.js
+++ Express js/src/MVC Architecture with node js/controller/userController.js
@@ -1,3 +1,8 @@
+// controller will attach view and model
+// model se data array arhi ha 
+// or user.ejs view ha or template engine ejs use ho rha ha or user.ejs ma data ja rha ha
+
+
 import { usersList } from "../model/userModel.js"
 
 export function handleusers(req,resp){
