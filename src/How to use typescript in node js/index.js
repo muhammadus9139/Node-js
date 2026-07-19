cad2192b0fd2dc8609faa2ea334a419674c84232
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-19 19:13:04 +0500

    Create index.js

---
+++ src/How to use typescript in node js/index.js
@@ -0,0 +1,15 @@
+console.log("Hello TypeScript with Node.js");
+
+const name: string = "Usama";
+const age: number = 21;
+const isStudent: boolean = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+console.log("Sum:", add(10, 20));
